061637585dc195d12bdc7cc6dc1529b7fa756aff
jbac208 <jbac208@aucklanduni.ac.nz>
2023-03-13 21:19:35 +1300

    implemented added profile message

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -21,6 +21,7 @@ public class InsuranceSystem {
 
   public void createNewProfile(String userName, String age) {
     // TODO: Complete this method.
+    userName = toTitle(userName);
     if (isInDatabase(userName)) {
       // profile with username already exists within database
       MessageCli.INVALID_USERNAME_NOT_UNIQUE.printMessage(userName);
@@ -29,6 +30,7 @@ public class InsuranceSystem {
       // unique profile -> create new profile and add to database
       Profile profile = new Profile(userName, Integer.parseInt(age));
       database.add(profile);
+      MessageCli.PROFILE_CREATED.printMessage(userName, age);
     }
   }
 
