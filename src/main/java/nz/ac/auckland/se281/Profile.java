b271ec0fc703bf09a4bd2ef7b473443b8afb8ce3
jbac208 <jbac208@aucklanduni.ac.nz>
2023-03-17 16:58:19 +1300

    fixed formatting error, changed settings.json back to google java format

--- src/main/java/nz/ac/auckland/se281/Profile.java
+++ src/main/java/nz/ac/auckland/se281/Profile.java
@@ -2,23 +2,23 @@ package nz.ac.auckland.se281;
 
 public class Profile {
 
-    // instance fields
-    private String name;
-    private int age;
+  // instance fields
+  private String name;
+  private int age;
 
-    public Profile(String name, int age) {
-        // constructor
-        this.name = name;
-        this.age = age;
-    }
+  public Profile(String name, int age) {
+    // constructor
+    this.name = name;
+    this.age = age;
+  }
 
-    public String getUserName() {
-        // get name
-        return this.name;
-    }
+  public String getUserName() {
+    // get name
+    return this.name;
+  }
 
-    public int getAge() {
-        // get age
-        return this.age;
-    }
+  public int getAge() {
+    // get age
+    return this.age;
+  }
 }
